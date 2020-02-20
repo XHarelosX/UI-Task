d665ddd5da6dfd3b0937ee1a6f3484bff6cb6e74
Harel Asaf <harelasaf1@gmail.com>
2020-02-20 16:15:56 +0200

    Add Mock data

---
+++ src/Components/ListsComponents/ActivityMockData/Activity.ts
@@ -0,0 +1,35 @@
+export const activity: any[] = [
+    {
+        message1: {
+            Userame: "Nina Jones",
+            datetime: " Just now",
+            activity: "add a new project",
+            activity_on_task: "Free UI Kit"
+        }
+    },
+    {
+        message2: {
+            Userame: "James Smith",
+            datetime: "40 minutes ago",
+            activity: "Curabitur aliquet quam id dui posuere blandit.",
+            activity_on_task: "Free PSD Templane"
+        }
+    },
+    {
+        message3: {
+            Userame: "Alex Clooney",
+            datetime: "1 hour ago",
+            activity:
+                "Curabitur non nulla sit amet nisl tempus convallis quis ac lectus.",
+            activity_on_task: "Symu Website"
+        }
+    },
+    {
+        message4: {
+            Userame: "Alexandra Spears",
+            datetime: "3 hours ago",
+            activity: "Nulla quis lorem ut libero malesuada feugiat.",
+            activity_on_task: "Free PSD"
+        }
+    }
+];
--- src/Components/ListsComponents/MessagesMockData/Messages.ts
+++ src/Components/ListsComponents/MessagesMockData/Messages.ts
@@ -2,21 +2,21 @@ export const messages: any[] = [
     {
         message1: {
             Userame: "Nina Jones",
-            datetime: "",
+            datetime: "5 minutes ago",
             text_message: "Donec sollicitudin molestie malesuadau."
         }
     },
     {
         message2: {
             Userame: "Nina Jones",
-            datetime: "",
+            datetime: "About 20 hours ago",
             text_message: "Curabitur aliquet quam id dui posuere blandit."
         }
     },
     {
         message3: {
-            Userame: "N",
-            datetime: "",
+            Userame: "James Smith",
+            datetime: "2 days ago",
             text_message:
                 "Curabitur non nulla sit amet nisl tempus convallis quis ac lectus."
         }
@@ -24,7 +24,7 @@ export const messages: any[] = [
     {
         message4: {
             Userame: "Nina Jones",
-            datetime: "",
+            datetime: "About 2 weeks ago",
             text_message: "Nulla quis lorem ut libero malesuada feugiat."
         }
     }
