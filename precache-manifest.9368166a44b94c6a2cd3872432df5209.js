d26e81c792890bf3299a6bfdaa30ce9e78c28cc8
Harel Asaf <harelasaf1@gmail.com>
2020-02-23 03:20:29 +0200

    Updates

---
+++ precache-manifest.9368166a44b94c6a2cd3872432df5209.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ce138a6f9560037320a67a8789e759e4",
+    "url": "/UI-Task/index.html"
+  },
+  {
+    "revision": "1d2702aad328ba1d239c",
+    "url": "/UI-Task/static/css/main.08557f66.chunk.css"
+  },
+  {
+    "revision": "33fba2b1067fae73f751",
+    "url": "/UI-Task/static/js/2.f892d719.chunk.js"
+  },
+  {
+    "revision": "2a6f7b0ac760b5d15fb8b482e9b9b618",
+    "url": "/UI-Task/static/js/2.f892d719.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1d2702aad328ba1d239c",
+    "url": "/UI-Task/static/js/main.d78dde2a.chunk.js"
+  },
+  {
+    "revision": "7c2b46c57ffde7dc0d1d",
+    "url": "/UI-Task/static/js/runtime-main.517149b8.js"
+  }
+]);
